02b208866ab9dea708e64d3ef0476aa5bdfa54d5
todd-estes <elestes@gmail.com>
2021-03-08 11:55:53 -0700

    Refactor _fire_bullet method

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,12 +23,7 @@ class AlienInvasion:
           self._check_events()
           self.ship.update()
           self.bullets.update()
-          #Get rid of bullets that have disappeared 
-          for bullet in self.bullets.copy(): 
-              if bullet.rect.bottom <= 0: 
-                  self.bullets.remove(bullet)
-          print (len(self.bullets))
-          
+          self._update_bullets()
           self._update_screen()      
 
   def _check_events(self):
@@ -63,9 +58,19 @@ class AlienInvasion:
 
   def _fire_bullet(self): 
       #Create new bullet and add it to the bullets group 
-      new_bullet = Bullet(self)
-      self.bullets.add(new_bullet)
+      if len(self.bullets) < self.settings.bullets_allowed:
+          new_bullet = Bullet(self)
+          self.bullets.add(new_bullet)
 
+  def _update_bullets(self): 
+      #Update position of bullets and get rid of old bullets
+      #Update bullet positions  
+      self.bullets.update()
+
+      #Get rid of bullets that have disappeared
+      for bullet in self.bullets.copy():
+          if bullet.rect.bottom <= 0:
+              self.bullets.remove(bullet)
 
   def _update_screen(self): 
       #Redraw the screen during each pass through the loop
