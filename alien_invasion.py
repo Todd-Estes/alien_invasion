dc652cd27fb0c4b8e697c3ae06618ad7dadd148e
todd-estes <elestes@gmail.com>
2021-03-13 20:58:42 -0700

    Add _ship_hit method to alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys 
+from time import sleep
 import pygame 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship 
 from bullet import Bullet
 from alien import Alien
@@ -15,6 +17,7 @@ class AlienInvasion:
       self.settings.screen_height = self.screen.get_rect().height
       pygame.display.set_caption("Alien Invasion")
 
+      self.stats = GameStats(self)
       self.ship = Ship(self)
       self.bullets = pygame.sprite.Group()
       self.aliens = pygame.sprite.Group()
@@ -99,7 +102,24 @@ class AlienInvasion:
 
       #Look for alien-ship collisions 
       if pygame.sprite.spritecollideany(self.ship, self.aliens): 
-          print("Ship hit!!!")
+          self._ship_hit()
+
+  def _ship_hit(self): 
+      # Respond to the ship being hit by an alien  
+      # Decrement ships left 
+      self.stats.ships_left -= 1
+
+      # Get rid of any remaining aliens and bullets
+      self.aliens.empty()
+      self.bullets.empty()
+
+      # Create a new fleet and center the ship 
+      self._create_fleet()
+      self.ship.center_ship()
+
+      # Pause 
+      sleep(0.5)
+
             
   def _create_fleet(self): 
       #Create a fleet of aliens
