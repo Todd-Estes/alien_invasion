9542449ad55cd88d99ab84a871f3b13a170ef238
todd-estes <elestes@gmail.com>
2021-03-14 18:45:30 -0600

    Add check_high_score IOT render high score

--- scoreboard.py
+++ scoreboard.py
@@ -36,8 +36,7 @@ class Scoreboard:
         #Turn the high score into a rendered image 
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True,
-            self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
         #Center the high score at the top of the screen 
         self.high_score_rect = self.high_score_image.get_rect()
@@ -48,4 +47,4 @@ class Scoreboard:
         #Check to see if there's a new high score
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score 
-            self.prep_high_score
+            self.prep_high_score()
