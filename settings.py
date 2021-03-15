34fb1d797c1b6268f750343949c9a4d61f085fed
todd-estes <elestes@gmail.com>
2021-03-14 19:45:50 -0600

    Make permanent changes to Settings class

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         #Bullet settings 
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3 
@@ -30,7 +30,7 @@ class Settings:
         #Initialize settings that change throughout the game
         self.ship_speed = 1.5
         self.bullet_speed = 3.0 
-        self.alien_speed = 1.0 
+        self.alien_speed = 0.5
 
         #Fleet direction of 1 represents rights; -1 represents left
         self.fleet_direction = 1 
